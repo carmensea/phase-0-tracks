190426d376c9103bf2534f622ec7d1c706ab6732
Carmen Cojocariu <cojocariucarmen@gmail.com>
2017-06-19 22:07:07 -0700

    Two loops not iterating through second

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -24,16 +24,29 @@ while counter <= number_employees.to_i - 1
   puts "Would you like to enroll in the company's health insurance?"
   wants_healthcare = gets.chomp
 
-  if name == "Drake Cula" || name =="Tu Fang"
-    puts "Definitely a vampire"
-  elsif(current_year - birth_year.to_i == age.to_i) && (wants_garlic == "yes" || wants_healthcare == "yes")
-    puts "Probably not a vampire"
-  elsif (current_year - birth_year.to_i != age.to_i) && (wants_garlic == "no" && wants_healthcare == "no")
-    puts "Almost certainly a vampie"
-  elsif (current_year - birth_year.to_i != age.to_i) && (wants_garlic == "no" || wants_healthcare == "no")
-    puts "Probably a vampire"
-  else
-    print "Results inconclusive."
+  condition = true
+
+  while condition
+    puts "Please list any allergies one at a time. Please type done when finished"
+    allergies = gets.chomp
+    if allergies == "sunshine"
+      condition = false
+    elsif allergies == "done"
+      condition = false
+    end
   end
-  counter += 1
-end
+
+    if name == "Drake Cula" || name =="Tu Fang" || allergies == "sunshine"
+      puts "Definitely a vampire"
+    elsif(current_year - birth_year.to_i == age.to_i) && (wants_garlic == "yes" || wants_healthcare == "yes")
+      puts "Probably not a vampire"
+    elsif (current_year - birth_year.to_i != age.to_i) && (wants_garlic == "no" && wants_healthcare == "no")
+      puts "Almost certainly a vampie"
+    elsif (current_year - birth_year.to_i != age.to_i) && (wants_garlic == "no" || wants_healthcare == "no")
+      puts "Probably a vampire"
+    else
+      print "Results inconclusive."
+  end
+    counter += 1
+  end
+
