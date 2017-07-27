c50d2c829c7f119ab5952678fcd1c5ecc9030f0a
Carmen Cojocariu <cojocariucarmen@gmail.com>
2017-07-27 11:22:09 -0700

    Add driver code

--- ruby/build-something/phonebook.rb
+++ ruby/build-something/phonebook.rb
@@ -19,13 +19,40 @@ SQL
 #create the phonebook table
 db.execute(create_table_cmd)
 
-
 def create_people(db, name, age, phone)
   db.execute("INSERT INTO phonebook (name, age, phone) VALUES (?, ?, ?)", [name, age, phone])
 end
 
-def update_age(db, name, age)
-  db.execute("UPDATE phonebook SET age=#{age} WHERE name='#{name}'")
+def update_age(db, name, update)
+  db.execute("UPDATE phonebook SET age=#{update} WHERE name='#{name}'")
+end
+
+def update_phone(db, name, update)
+  db.execute("UPDATE phonebook SET phone=#{update} WHERE name='#{name}'")
+end
+
+def delete_person(db, name)
+  db.execute("DELETE name, age, phone FROM phonebook WHERE name='#{name}'")
+end
+
+def array_maker(db, information)
+  info_array = information.split(',')
+  info_parser(db, info_array)
+end
+
+def info_parser(db, array)
+  name = array[0]
+  age = array[1]
+  phone = array[2]
+  add_person(db, name, age, phone)
+end
+
+def add_person(db, name, age, phone)
+  db.execute("INSERT INTO phonebook (name, age, phone) VALUES (?, ?, ?)", [name, age, phone])
+end
+
+def return_user(db, name)
+  db.execute("SELECT name, age, phone from phonebook WHERE name ='#{name}'")
 end
 
 #10.times do
@@ -38,13 +65,33 @@ end
 puts "Would you like to make a change?"
 input = gets.chomp
 if input == "yes"
-  puts "Who would you like to update?"
-  name_input = gets.chomp
-  puts "What would you like to update?"
-  field = gets.chomp
-  puts "What would you like to update #{field} to?"
-  update = gets.chomp
-  update_age(db, name_input, update)
+  puts "What would you like to do (add/delete/update)?"
+  do_this = gets.chomp
+    if do_this == "update"
+      puts "Whom would you like to update?"
+      name_input = gets.chomp
+      puts "What would you like to update?"
+      field = gets.chomp
+      puts "What would you like to update #{field} to?"
+      update = gets.chomp
+        if field == "age"
+          update_age(db, name_input, update)
+          puts return_user(db, name_input)
+        elsif field == "phone number"
+          update_phone(db, name_input, update)
+          puts return_user(db, name_input)
+        end
+    elsif do_this == "delete"
+      puts "Whom would you like to delete?"
+      name_input = gets.chomp
+      delete_person(db, name_input)
+    elsif do_this == "add"
+      puts "Please give me the name, age, and phone number of the addition (separated by commas)."
+      addition = gets.chomp
+      array_maker(db, addition)
+    end
+else
+  puts "Great! Glad I could help!"
 end
 
 
